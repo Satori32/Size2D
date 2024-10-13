68db3942f323adef42ee4928d3d4280b09eb569d
Satori32 <Huga8989+githubf@gmail.com>
2024-10-14 08:26:50 +0900

    forgot uodate header

--- Size2D.h
+++ Size2D.h
@@ -14,3 +14,4 @@ template<class T> bool ResizeX(Size2D<T>& In, T V);
 template<class T> bool ResizeY(Size2D<T>& In, T V);
 template<class T> T Width(Size2D<T>& In);
 template<class T> T Height(Size2D<T>& In);
+template<class T> bool ToZero(Size2D<T>& In);
